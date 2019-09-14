b6444e42de7dc706c5577522937f9352b4e7d288
Ron Ganguly <c1ph3r9@gmail.com>
2019-09-14 14:38:55 -0500

    add session feature to persist info

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express=require("express")
+const session=require("express-session")
 const router=require("./router")
 const app=express()
+let sessionOptions=session({
+  secret:"Javascript is so cool",
+  resave:false,
+  saveUninitialized:false,
+  cookie:{
+    maxAge:1000*60*60*24*7,httpOnly:true
+  }
+})
+app.use(sessionOptions)
 //html form submit and get the data from req. body
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,9 @@ const User=require("../models/User")
 exports.login=function(req,res){
   let user=new User(req.body)
   user.login().then((result)=>{
+    req.session.user={username:user.data.username}
+    console.log(user);
+    
   res.send(result)
   }).catch(err=>{
     res.send(err)
@@ -21,5 +24,9 @@ exports.register=function (req,res){
   }
 }
 exports.home=function(req,res){
-  res.render('home-guest')
+  if(req.session.user){
+    res.send("Welcome to the app")
+  }else{
+    res.render('home-guest')
+  }
 }
