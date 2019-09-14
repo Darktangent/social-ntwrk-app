956937d327dc100a662389afe293d46f38c77cb2
Ron Ganguly <c1ph3r9@gmail.com>
2019-09-14 10:15:30 -0500

    added login feature and hashing for both register and login fn

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const validator =require("validator")
+const bcrypt=require("bcryptjs")
 const usersCollection= require('../db').collection("users")
 let User=function(data){
 this.data=data
@@ -21,8 +22,8 @@ User.prototype.validate=function(){
   if(this.data.password.length>0 && this.data.password.length<12){
     this.errors.push("Password must be atleast 12 characters")
   }
-  if(this.data.password.length>100){
-    this.errors.push("Passwords cannot exceed 100 characters")
+  if(this.data.password.length>50){
+    this.errors.push("Passwords cannot exceed 50 characters")
   } 
   if(this.data.username.length>0 && this.data.username.length<3){
     this.errors.push("Username must be atleast 3 characters")
@@ -32,16 +33,20 @@ User.prototype.validate=function(){
   }
   
 }
-User.prototype.login = function(callback){
-  this.cleanUp()
-  usersCollection.findOne({username:this.data.username},(err,attemptedUser)=>{
-    if(attemptedUser && attemptedUser.password==this.data.password){
-     callback("congrats")
-      
-    }else{
-     callback("invalid username/password")
-      
-    }
+User.prototype.login = function(){
+  return new Promise((resolve,reject)=>{
+    this.cleanUp()
+    usersCollection.findOne({username:this.data.username}).then((attemptedUser)=>{
+      if(attemptedUser && bcrypt.compareSync(this.data.password,attemptedUser.password)){
+        resolve("congrats")
+       
+     }else{
+      reject("invalid username/password")
+       
+     }
+    }).catch(()=>{
+      reject("Please try again later")
+    })
   })
 }
 User.prototype.cleanUp=function(){
@@ -64,10 +69,13 @@ User.prototype.cleanUp=function(){
 User.prototype.register=function(){
   // step 1 validate user data
  
-  
+  this.cleanUp()
   this.validate()
   // step 2 only if there's no validation erro save user data into Database
   if(!this.errors.length){
+    // hash user password
+    let salt=bcrypt.genSaltSync(10)
+    this.data.password=bcrypt.hashSync(this.data.password,salt)
     usersCollection.insertOne(this.data)
   }
 }
