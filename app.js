6150ffaf476ae1a2092ef8b9dd533c25be2dcab9
Ron Ganguly <c1ph3r9@gmail.com>
2019-09-20 12:19:17 -0500

    add delete individual post by owner feature

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-const express=require("express")
-const session=require("express-session")
-const flash=require("connect-flash")
-const MongoStore=require("connect-mongo")(session)
+const express = require('express')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const flash = require('connect-flash')
+const markdown = require('marked')
+const sanitizeHTML=require('sanitize-html')
+const app = express()
 
-const app=express()
-let sessionOptions=session({
-  secret:"Javascript is so cool",
-  store:new MongoStore({client:require('./db')}),
-  resave:false,
-  saveUninitialized:false,
-  cookie:{
-    maxAge:1000*60*60*24*7,httpOnly:true
-  }
+let sessionOptions = session({
+  secret: "JavaScript is sooooooooo coool",
+  store: new MongoStore({client: require('./db')}),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {maxAge: 1000 * 60 * 60 * 24, httpOnly: true}
 })
+
 app.use(sessionOptions)
 app.use(flash())
-app.use(function(req,res,next){
-  // make userid available
-  if(req.session.user){
-    req.visitorId=req.session.user._id
-  }else{
-    req.visitorId=0
+
+app.use(function(req, res, next) {
+  // make markdown available within templates
+  res.locals.filterUserHTML=function(content){
+    return sanitizeHTML(markdown(content),{allowedTags:["p","br","ul",'ol','li','strong','bold','em','h1','h2','h3'],allowedAttributes:{}})
   }
-  // make user session available from within view templates
-  res.locals.user=req.session.user
+  // make all error and success flash messages available from all templates
+  res.locals.errors = req.flash("errors")
+  res.locals.success = req.flash("success")
+
+  // make current user id available on the req object
+  if (req.session.user) {req.visitorId = req.session.user._id} else {req.visitorId = 0}
+  
+  // make user session data available from within view templates
+  res.locals.user = req.session.user
   next()
 })
-const router=require("./router")
-//html form submit and get the data from req. body
+
+const router = require('./router')
+
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
-
 app.use(express.static('public'))
-app.set('views','views')
+app.set('views', 'views')
 app.set('view engine', 'ejs')
 
-app.use("/",router)
+app.use('/', router)
 
-
-
-module.exports=app
+module.exports = app
